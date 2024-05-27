5016a422c784feef1bb3dbab86b8287eba82a4c8
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2024-05-27 20:42:45 +0300

    CopyAssignment DONE

--- IntroductionToOOP/IntroductionToOOP/IntroductionToOOP.cpp
+++ IntroductionToOOP/IntroductionToOOP/IntroductionToOOP.cpp
@@ -1,9 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
+
 #define tab "\t"
+#define delimiter "\n----------------------------------------------------------------------\n"
+
+
 //#define STRUCT_POINT // CTRL + SPACE
-#define DISTANCE_CHECK
+//#define DISTANCE_CHECK
 //#define CONSTRUCTORS_CHECK
+#define ASSINGMENT_CHECK
 
 //Создавая структуру или класс, мы создаем новый тип данных
 class Point
@@ -28,7 +33,7 @@ public:
 		this->y = y;
 	}
 
-	//	Constructors
+	//		Constructors
 
 	Point()
 	{
@@ -58,18 +63,27 @@ public:
 		cout << "Destructor:\t\t" << this << endl;
 	}
 
-	//	Methods
-	double distance(Point& other)const
+	//		Operators:
+	Point& operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+
+	//		Methods
+	double distance(const Point& other)const //константный объект нельзя изменить, this и other теперь константные
 	{
 		return sqrt(pow((other.x - this->x), 2) + pow((other.y - this->y), 2));
 	}
 	void print()const
 	{
-		cout << "X = " << x << tab << "Y = " << y << endl;
+		cout << this << ": X = " << x << tab << "Y = " << y << endl;
 	}
 };
 
-double distance(Point& A, Point& B);
+double distance(const Point& A, const Point& B);
 
 int main()
 {
@@ -98,10 +112,13 @@ int main()
 	Point B;
 	B.set_x(7);
 	B.set_y(8);
-	cout << distance(A, B) << tab << distance(B, A) << endl;
 	cout << A.get_x() << tab << A.get_y() << endl;
-	cout << B.get_x() << tab << B.get_y() << endl;
-	cout << A.distance(B) << tab << B.distance(A) << endl;
+	cout << B.get_x() << tab << B.get_y() << delimiter;
+
+	cout << distance(A, B) << delimiter;
+	cout << distance(B, A) << delimiter;
+	cout << A.distance(B) << delimiter;
+	cout << B.distance(A) << delimiter;
 #endif // DISTANCE_CHECK
 
 #ifdef CONSTRUCTORS_CHECK
@@ -118,9 +135,29 @@ int main()
 	D.print();
 #endif // CONSTRUCTORS_CHECK
 
+#ifdef ASSINGMENT_CHECK
+	//Point A(2, 3); //Constructor
+	//Point B;	   //Default constructor
+	//B = A;		   //Copy assignment
+	//B.print();
+	//Point C = B;   //CopyConstructor
+
+	int a, b, c;
+	a = b = c = 0; // c = 0, b = c, a = b // Ассоциативность справа налево
+	cout << a << tab << b << tab << c << endl;
+
+	Point A, B, C;
+	cout << delimiter << endl;
+	A = B = C = Point(2, 3);
+	cout << delimiter << endl;
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSINGMENT_CHECK
+
 }
 
-double distance(Point& A, Point& B)
+double distance(const Point& A, const Point& B)
 {
 	//A(xa, ya), B(xb, yb): AB = √((xb - xa)^2 + (yb - ya)^2)
 	return sqrt(pow((B.get_x() - A.get_x()), 2) + pow((B.get_y() - A.get_y()), 2));

dcd1bfa53e8f085eb9caebc8860ab4a3726af96f
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2024-06-14 18:36:59 +0300

    MoveConstructor/Assignment added

--- IntroductionToOOP/String/main.cpp
+++ IntroductionToOOP/String/main.cpp
@@ -13,85 +13,112 @@ public:
 	{
 		return size;
 	}
-	char* get_str()const
+	const char* get_str()const //const char*, чтобы не измен€лс€ объект, т.к. возвращаетс€ указатель на него
 	{
 		return str;
 	}
+	void set_str(char* str)
+	{
+		this->str = str;
+		this->size = strlen(str);
+	}
+	void set_size(int size)
+	{
+		this->size = size;
+	}
+
 	//		Constructors:
+
 	String(int size = 80)
 	{
 		this->size = size;
 		this->str = new char[size] {};
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
-	String(const char arr[], int size = 80) //считаетс€ 1arg, или уже нет?
+	String(const char str[])
 	{
-		this->size = size;
-		this->str = new char[size] {};
-		for (int i = 0; arr[i] != '\0'; i++)
-			str[i] = arr[i];
+		//strlen(), чек —-функции 
+		this->size = strlen(str) + 1;
+		this->str = new char[size] {}; // инициализаци€ {} в конце строки проставл€ет ноль
+		for (int i = 0; str[i] != '\0'; i++) this->str[i] = str[i];
 		cout << "1ArgConstructor:\t" << this << endl;
 	}
-	String(const char line1[], const char line2[], int size = 160)
+	String(const char line1[], const char line2[])
 	{
-		this->size = size;
+		this->size = strlen(line1) + strlen(line2) + 1;
 		this->str = new char[size] {};
 		int index = 0;
-		for (int i = 0; line1[i] != '\0'; i++, index++)
-			str[i] = line1[i];
-		for (int i = 0; line2[i] != '\0'; i++, index++)
-			str[index] = line2[i];
-		cout << "Constructor:\t" << this << endl;
+		for (int i = 0; line1[i] != '\0'; i++, index++) str[i] = line1[i];
+		for (int i = 0; line2[i] != '\0'; i++, index++) str[index] = line2[i];
+		cout << "Constructor:\t\t" << this << endl;
 	}
 	String(const String& obj)
 	{
 		size = obj.size;
 		str = new char[obj.size] {};
-		for (int i = 0; obj.str[i] != '\0'; i++)
-			str[i] = obj.str[i];
+		for (int i = 0; obj.str[i] != '\0'; i++) str[i] = obj.str[i];
 		cout << "CopyConstructor:\t" << this << endl;
+	}
+	String(String&& obj)
+	{
+		this->str = obj.str;
+		this->size = obj.size;
+		obj.str = nullptr;
+		obj.size = 0;
+		cout << "MoveConstructor:\t" << this << endl;
 	}
 	~String()
 	{
 		delete[] this->str;
 		cout << "Destructor:\t\t" << this << endl;
 	}
+
+	//		Operators:
+
+	String& operator=(const String& obj)
+	{
+		size = obj.size;
+		str = new char[obj.size] {};
+		for (int i = 0; obj.str[i] != '\0'; i++) str[i] = obj.str[i];
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+	String& operator=(String&& obj)
+	{
+		delete[] this->str;
+		this->str = obj.str;
+		this->size = obj.size;
+		obj.str = nullptr;
+		obj.size = 0;
+		cout << "MoveAssignment:\t\t" << this << endl;
+		return *this;
+	}
+
 	//		Methods:
+
 	void print()const
 	{
 		cout << "Size:\t" << size << endl;
 		cout << "Str:\t" << str << endl;
 	}
-	String& operator=(const String& obj)
-	{
-		size = obj.size;
-		str = new char[obj.size] {};
-		for (int i = 0; obj.str[i] != '\0'; i++)
-			str[i] = obj.str[i];
-		cout << "CopyAssignment:\t\t" << this << endl;
-		return *this;
-	}
 };
 String operator+(const String& left, const String& right)
 {
-	//char* buffer = new char[left.get_size() + right.get_size()] {};
-	//int index = 0;
-	//for (; index < left.get_size(); index++)
-	//	buffer[index] = left.get_str()[index];
-	//for (; index < left.get_size() + right.get_size(); index++)
-	//	buffer[index] = left.get_str()[index];
-	return String
-	(
-		left.get_str(),
-		right.get_str(),
-		left.get_size() + right.get_size()
-	);
+	return String(left.get_str(), right.get_str());
 }
-std::ostream& operator<<(std::ostream& os, const String& str)
+std::ostream& operator<<(std::ostream& os, const String& obj) //&, т.к. потоки копировать нельз€
 {
-	os << "Size:\t" << str.get_size() << endl; 
-	os << "Str:\t" << str.get_str(); //почему, обраща€сь к адресу начала строки в пам€ти, мы получаем ее всю на выводе?
-	return os;
+	//?: почему, обраща€сь к адресу начала строки в пам€ти, мы получаем ее всю на выводе?
+	//&: т.к. << перегружен дл€ массива char
+	return os << obj.get_str();
+}
+istream& operator>>(istream& is, String& obj)
+{
+	char* temp = new char[80] {};
+	is >> temp;
+	obj.set_str(temp);
+	temp = nullptr;
+	return is;
 }
 
 int main()
@@ -99,7 +126,7 @@ int main()
 	setlocale(LC_ALL, "");
 
 	String str1 = "Hello";
-	String str2("World", 6); //на 5 элементов позвол€ет, на 4 Heap Corruption при вызове деструктора
+	String str2("World");    //на 5 элементов позвол€ет, на 4 Heap Corruption при вызове деструктора
 							 //то есть при 5 элементах затираетс€ лишь нуль, а на 4 уже и брейк поинт?
 							 // 
 							 //почему при затертом нуле выводит не 80 элементов белиберды, а лишь несколько?
@@ -119,4 +146,27 @@ int main()
 
 	String str3 = str1 + str2;
 	cout << str3 << endl;
+
+	cout << delimiter << endl;
+
+	String hola = "Hola";
+	//String str4 = hola;			//почему отрабатывает 1arg только единожды? почему не вызываетс€ CC
+	String str4 = String("Hola");	//Ќе отрабатывает Move, хот€ говоритс€, что Move примен€етс€
+									//при использовании в инициализации безым€нных объектов
+
+	/*https://legacy.cplusplus.com/doc/tutorial/classes2/
+	Compilers already optimize many cases that formally require a 
+	move-construction call in what is known as Return Value Optimization.
+	Most notably, when the value returned by a function is used to initialize an object.
+	In these cases, the move constructor may actually never get called.*/
+	
+	String str5;
+	str5 = str4 + String("amigos");
+	str5.print();
+
+	cout << delimiter << endl;
+
+	String input_test;
+	cin >> input_test;
+	input_test.print();
 }
